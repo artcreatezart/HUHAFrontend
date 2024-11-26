5049d9b14275802e6b9aa78c23ae89fd7170d471
artcreatezart <abbie@Abbies-MacBook-Pro.local>
2024-11-27 12:31:58 +1300

    added more home content in, having issue with getfeaturedimage on campaigns, tried fixing in campaign component and custom theme

---
+++ src/components/Campaign.jsx
@@ -0,0 +1,60 @@
+import{ useState, useEffect } from 'react'
+import axios from 'axios'
+
+const baseURL = import.meta.env.VITE_WP_API_BASEURL
+
+const Campaign = () => {
+  const [campaign, setCampaign] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const endpoint = `${baseURL}/campaign?_embed`
+
+  useEffect(() => {
+    axios.get( `${ endpoint }`)
+    .then(( response ) => {
+      setCampaign( response.data );
+      setLoading( false );
+    }) 
+    .catch(( error ) => console.log( error ))
+  }, [])
+
+  function getFeaturedImage( campaign ) {
+    if (campaign && campaign._embedded && campaign._embedded['wp:featuredmedia'] && campaign._embedded['wp:featuredmedia'][0].source_url) {
+      return campaign._embedded['wp:featuredmedia'][0].source_url;
+  } else {
+      return 'https://via.placeholder.com/150';
+  }
+  }
+
+  if (loading) {
+    return <>Loading...</>
+  }
+  
+  return (
+    <div className='campaign-container'>
+      {campaign.map((campaign, index) => (
+        <div key={campaign.slug + '-' + index} className='campaign-card'>
+          <div className='left-img-campaign-description'>
+            <img
+              src={getFeaturedImage(campaign)}
+              alt={campaign.title.rendered}
+            />
+          </div>
+          
+          <div className='right-text-campaign-description'>
+            <h4 className='title'>{campaign.title.rendered}</h4>
+            <p dangerouslySetInnerHTML={{ __html: campaign.content.rendered }} />
+            <a href={campaign.acf.link_to_campaign__petition} target='_blank'>
+              <button className='secondary-button'>
+                Sign The Petition
+              </button>
+            </a>
+          </div>
+          
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Campaign
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,69 @@
 import React from 'react'
-import HomeHeader from '../components/HomeHeader'
-
+import HomeHeader from '../components/aboutComponents/HomeHeader'
+import Campaign from '../components/Campaign'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
       <HomeHeader/>
-      <div>
+      <div className='huha-home-section'>
+
+        <div className='huha-description'>
+
+          <div className='left-text-huha-description'>
+            <h3>HUHA is a leading animal rescue supporting the welfare of dogs, cats, horses, and other animals across New Zealand.</h3>
+            <p>HUHA - Helping You Help Animals - is a charitable trust (#CC47396) and New Zealand’s leading no-kill animal shelter. Since our founding in 2001, we’ve advocated for, rescued, rehabilitated, rehomed and released tens of thousands of domestic and wild animals in need. Together, we save lives!</p>
+          </div>
+
+          <div className='right-img-huha-description'/>
+
+        </div>
+
+        <div className='home-advice-section'>
+          <h1>Advice</h1>
+          <div className='home-advice-card-container'>
+            {/* Advice Card #1 */}
+            <div className='advice-card'>
+              <div className='advice-card-img'/>
+              <h3>Hurt | Injured| Lost</h3>
+              <Link to={`/support`}>
+              <button className='primary-button'>Find Out More</button>
+              </Link>
+              
+            </div>
+
+            {/* Advice Card #2 */}
+            <div className='advice-card'>
+              <div className='advice-card-img'>
+              </div>
+              <h3>Rehome</h3>
+              <button className='primary-button'>Find Out More</button>
+            </div>
+
+            {/* Advice Card #3 */}
+            <div className='advice-card'>
+              <div className='advice-card-img'/>
+              <h3>Disaster Preparation</h3>
+              <button className='primary-button'>Find Out More</button>
+            </div>
+
+            {/* Advice Card #4 */}
+            <div className='advice-card'>
+              <div className='advice-card-img'/>
+              <h3>Abused Animal</h3>
+              <button className='primary-button'>Find Out More</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='campaign-section customize-section-color'>
+          <Campaign/>
+        </div>
+
       </div>
+      
+
     </>
     
   )
